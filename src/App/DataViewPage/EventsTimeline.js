91d049dfa37dc95bda7829866f9d750a00216ddb
Eliseev Aleksei <eliseev-eag@yandex.ru>
2020-09-03 09:54:59 +0700

    Реализовал отрисовку timeline без склеивания событий в линию

--- src/App/DataViewPage/EventsTimeline.js
+++ src/App/DataViewPage/EventsTimeline.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { extent } from 'd3-array';
-import { scaleBand, scaleTime } from 'd3-scale';
+import { extent, range } from 'd3-array';
+import { scaleBand, scaleOrdinal, scaleTime } from 'd3-scale';
+import { schemeSet2 } from 'd3-scale-chromatic';
 import { eventsSelector, eventTypesSelector } from 'selectors';
 import './index.css';
 
 const DEFAULT_WIDTH = 400;
 const DEFAULT_HEIGHT = 400;
 const MARGIN_LEFT = 50;
-const MARGIN_TOP = 50;
+const MARGIN_TOP = 0;
+const MARGIN_BOTTOM = 20;
 const TICK_LENGTH = 6;
-const RECTANGLE_HEIGHT = 30;
 
 const PIXELS_PER_TICK = 100;
 
@@ -42,11 +43,7 @@ const EventsTimeline = () => {
     eventTypes,
   ]);
 
-  const yScale = scaleBand()
-    .domain(evenTypesAsString)
-    .range([MARGIN_TOP, sizes.height - MARGIN_TOP])
-    .padding(0.1)
-    .round(true);
+  const getColor = scaleOrdinal(schemeSet2).domain(evenTypesAsString);
 
   const xScale = scaleTime()
     .domain(extent(domainDates))
@@ -63,41 +60,48 @@ const EventsTimeline = () => {
     xOffset: xScale(value),
   }));
 
-  useEffect(() => {
+  const eventsForDrawing = eventsWithTypes
+    .map((event) => {
+      const x1 = xScale(event.startDate);
+      const x2 = xScale(event.endDate);
+      const width = x2 - x1;
+      return { x1, x2, width, ...event };
+    })
+    .filter((event) => event.width > 10);
+
+  const yScale = scaleBand()
+    .domain(range(eventsForDrawing.length))
+    .range([MARGIN_TOP, sizes.height - MARGIN_BOTTOM])
+    .paddingOuter(0.3)
+    .paddingInner(0.1);
+
+  useLayoutEffect(() => {
     const { width, height } = svgRef.current.getBoundingClientRect();
     setSizes({ width, height });
   }, []);
 
   return (
     <svg className="timeline-container" ref={svgRef}>
-      {eventsWithTypes
-        .map((event) => {
-          const x1 = xScale(event.startDate);
-          const x2 = xScale(event.endDate);
-          const y1 = yScale(event.type);
-          const width = x2 - x1;
-          return { x1, x2, y1, width, ...event };
-        })
-        .filter((event) => event.width > 10)
-        .map((event) => (
-          <g key={event.id}>
-            <rect
-              x={event.x1}
-              width={event.width}
-              y={event.y1}
-              height={RECTANGLE_HEIGHT}
-            />
-            <text
-              x={event.x1 + event.width / 2}
-              y={event.y1 + RECTANGLE_HEIGHT / 2}
-              dominantBaseline="middle"
-              textAnchor="middle"
-              stroke="white"
-            >
-              {event.name}
-            </text>
-          </g>
-        ))}
+      {eventsForDrawing.map((event, index) => (
+        <g key={event.id}>
+          <rect
+            x={event.x1}
+            width={event.width}
+            y={yScale(index)}
+            height={yScale.bandwidth()}
+            fill={getColor(event.type)}
+          />
+          <text
+            x={event.x1 + event.width / 2}
+            y={yScale(index) + yScale.bandwidth() / 2}
+            dominantBaseline="middle"
+            textAnchor="middle"
+            style={{ fontSize: `${yScale.bandwidth()}px` }}
+          >
+            {event.name}
+          </text>
+        </g>
+      ))}
       <g className="x-axis">
         <path
           d={[
