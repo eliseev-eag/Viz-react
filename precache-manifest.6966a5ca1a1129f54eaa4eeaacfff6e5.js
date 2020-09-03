28b81fa330de828c2587953bf07ddccc7ec4360b
eliseev-eag <eliseev-eag@yandex.ru>
2020-09-03 12:24:06 +0000

    Deploying to gh-pages from master @ 6a16cb715e1a3fa01adeebf13a762304104bb38e 🚀

---
+++ precache-manifest.6966a5ca1a1129f54eaa4eeaacfff6e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511f50252bfe00ea3813757f3343a24f",
+    "url": "/Viz-react/index.html"
+  },
+  {
+    "revision": "45f23b246f40e4694778",
+    "url": "/Viz-react/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "3e009cdeaa7937d3645b",
+    "url": "/Viz-react/static/css/main.4df8a1b5.chunk.css"
+  },
+  {
+    "revision": "45f23b246f40e4694778",
+    "url": "/Viz-react/static/js/2.81406f34.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/Viz-react/static/js/2.81406f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7785ddcd3e2794419a36",
+    "url": "/Viz-react/static/js/3.6605ec34.chunk.js"
+  },
+  {
+    "revision": "3e009cdeaa7937d3645b",
+    "url": "/Viz-react/static/js/main.c874f478.chunk.js"
+  },
+  {
+    "revision": "ff3f983d65b6c8d5be6d",
+    "url": "/Viz-react/static/js/runtime-main.e32c68fb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Viz-react/precache-manifest.093660d8087425a54d51518d1e3484c1.js"
+  "/Viz-react/precache-manifest.6966a5ca1a1129f54eaa4eeaacfff6e5.js"
 );
 
 self.addEventListener('message', (event) => {
